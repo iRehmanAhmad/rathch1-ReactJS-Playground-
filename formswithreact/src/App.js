880ae57f813ac5fc9932d7b754789ef6e32ecb84
Rehman Ahmad <thepublicinterface@gmail.com>
2022-05-28 13:34:16 +0500

    useContext first time

--- formswithreact/src/App.js
+++ formswithreact/src/App.js
@@ -1,28 +1,32 @@
-import {useState, useEffect} from "react";
-
-
-function App() {
-  console.log("outside useEffect");
-
-  const [count, setCount] = useState(window.innerWidth);
-  function setFun(){
-    setCount(window.innerWidth);
-  }
-  useEffect(()=>{
-  window.addEventListener("resize",setFun); 
-  return ()=>{
-  console.log("cleanup operation")
-  window.removeEventListener("resize", setFun);
-  }
-  },[]);
-  <h1>I've rendered {count} times!</h1>;
-  return (
-    <>
-      
-      
-      <h1>{count}</h1> 
-    </>   
+import {useState, useContext, createContext} from "react";
+const UserContext = createContext();
+function Component1(){
+let[user, setUser] = useState("Rehman Ahmad");
+  return(
+  <>
+    <UserContext.Provider value={user}>
+      <h1>Hello {user}</h1>
+      <Component2 user={user} />
+    </UserContext.Provider>
+  </>
+);
+}
+function Component2(){
+  const user = useContext(UserContext);
+  return(
+  <>
+  <h1>Hello G {user}</h1>
+  <Component3/>
+  </>
+  );
+}
+function Component3(){
+  const user = useContext(UserContext);
+  return(
+  <>
+  <h1>Hello G {user}</h1>
+  </>
   );
 }
 
-export default App;
+export default Component1;
